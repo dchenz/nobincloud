3bda0b9e76097c2770d9696b0e940dec512b1648
Danny <51312422+dchenz@users.noreply.github.com>
2022-11-03 22:14:12 +1100

    web: Add unittest for utils.ts

---
+++ web-client/src/crypto/utils.test.ts
@@ -0,0 +1,44 @@
+import { concatArrayBuffer } from "./utils";
+
+test("Concat array buffer", () => {
+  const text1 = "hello world";
+  const text2 = "test";
+  const text3 = "";
+  const buf1 = Buffer.from(text1);
+  const buf2 = Buffer.from(text2);
+  const buf3 = Buffer.from(text3);
+
+  const concatToString = (...buffers: ArrayBuffer[]) =>
+    Buffer.from(concatArrayBuffer(...buffers)).toString("utf8");
+
+  // Single ArrayBuffer
+
+  expect(concatToString(buf1))
+    .toBe(text1);
+
+  expect(concatToString(buf3))
+    .toBe(text3);
+
+  // Two ArrayBuffer
+
+  expect(concatToString(buf1, buf2))
+    .toBe(text1 + text2);
+
+  expect(concatToString(buf2, buf1))
+    .toBe(text2 + text1);
+
+  // ArrayBuffer with itself
+
+  expect(concatToString(buf1, buf1))
+    .toBe(text1 + text1);
+
+  // More than two ArrayBuffer
+
+  expect(concatToString(buf1, buf2, buf2, buf1))
+    .toBe(text1 + text2 + text2 + text1);
+
+  // No ArrayBuffer
+
+  expect(concatToString())
+    .toBe("");
+});
