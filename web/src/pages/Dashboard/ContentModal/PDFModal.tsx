2856cf416609a0ef70b38c02bfde9b97354bb96e
Danny <51312422+dchenz@users.noreply.github.com>
2023-01-02 20:36:54 +1100

    feat: Add page buttons in PDF viewer

--- web/src/pages/Dashboard/ContentModal/PDFModal.tsx
+++ web/src/pages/Dashboard/ContentModal/PDFModal.tsx
@@ -1,6 +1,11 @@
 import { ButtonGroup, IconButton } from "@chakra-ui/react";
 import React, { useState } from "react";
-import { ZoomIn, ZoomOut } from "react-bootstrap-icons";
+import {
+  ChevronLeft,
+  ChevronRight,
+  ZoomIn,
+  ZoomOut,
+} from "react-bootstrap-icons";
 import { Document, Page, pdfjs } from "react-pdf";
 import { FileRef } from "../../../types/Files";
 import "./styles.scss";
@@ -17,12 +22,18 @@ const MAX_ZOOM = 2.0;
 
 const PDFModal: React.FC<PDFModalProps> = ({ bytes }) => {
   const [zoom, setZoom] = useState(1);
+  const [totalPages, setTotalPages] = useState(0);
+  const [pageNumber, setPageNumber] = useState(1);
+
   return (
     <React.Fragment>
-      <Document file={bytes}>
+      <Document
+        file={bytes}
+        onLoadSuccess={(doc) => setTotalPages(doc.numPages)}
+      >
         <Page
           className="react-pdf-page"
-          pageNumber={1}
+          pageNumber={pageNumber}
           scale={zoom}
           renderTextLayer={false}
           renderAnnotationLayer={false}
@@ -40,6 +51,20 @@ const PDFModal: React.FC<PDFModalProps> = ({ bytes }) => {
           aria-label="zoom-in"
           onClick={() => setZoom(Math.min(zoom + 0.25, MAX_ZOOM))}
           disabled={zoom === MAX_ZOOM}
+        />
+      </ButtonGroup>
+      <ButtonGroup position="absolute" top="70px">
+        <IconButton
+          icon={<ChevronLeft />}
+          aria-label="previous-page"
+          onClick={() => setPageNumber(Math.max(pageNumber - 1, 1))}
+          disabled={pageNumber === 1}
+        />
+        <IconButton
+          icon={<ChevronRight />}
+          aria-label="next-page"
+          onClick={() => setPageNumber(Math.min(pageNumber + 1, totalPages))}
+          disabled={pageNumber === totalPages}
         />
       </ButtonGroup>
     </React.Fragment>
