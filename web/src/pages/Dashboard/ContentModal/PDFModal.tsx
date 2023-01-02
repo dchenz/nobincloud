9952dc3c75010d92d0b3399f4483932065e945b3
Danny <51312422+dchenz@users.noreply.github.com>
2023-01-02 20:51:59 +1100

    fix: Better styles for zoom/page buttons in PDF viewer

--- web/src/pages/Dashboard/ContentModal/PDFModal.tsx
+++ web/src/pages/Dashboard/ContentModal/PDFModal.tsx
@@ -1,5 +1,5 @@
-import { ButtonGroup, IconButton } from "@chakra-ui/react";
-import React, { useState } from "react";
+import { Box, ButtonGroup, IconButton, Text } from "@chakra-ui/react";
+import React, { useMemo, useRef, useState } from "react";
 import {
   ChevronLeft,
   ChevronRight,
@@ -24,9 +24,33 @@ const PDFModal: React.FC<PDFModalProps> = ({ bytes }) => {
   const [zoom, setZoom] = useState(1);
   const [totalPages, setTotalPages] = useState(0);
   const [pageNumber, setPageNumber] = useState(1);
+  const [showControls, setShowControls] = useState(false);
+  const navRef = useRef<HTMLDivElement>(null);
+
+  const onDocumentMouseMove = useMemo(() => {
+    let timer: NodeJS.Timeout | null = null;
+    return () => {
+      if (timer) {
+        clearTimeout(timer);
+      }
+      setShowControls(true);
+      timer = setTimeout(() => {
+        if (navRef.current) {
+          // HACK: If mouse is hovering over controls, it will
+          // have "border-top-style" set to "hidden" in CSS.
+          const bt = window
+            .getComputedStyle(navRef.current)
+            .getPropertyValue("border-top-style");
+          if (bt !== "hidden") {
+            setShowControls(false);
+          }
+        }
+      }, 3000);
+    };
+  }, [navRef.current]);
 
   return (
-    <React.Fragment>
+    <Box onMouseMove={onDocumentMouseMove}>
       <Document
         file={bytes}
         onLoadSuccess={(doc) => setTotalPages(doc.numPages)}
@@ -39,35 +63,52 @@ const PDFModal: React.FC<PDFModalProps> = ({ bytes }) => {
           renderAnnotationLayer={false}
         />
       </Document>
-      <ButtonGroup position="absolute" top="20px">
-        <IconButton
-          icon={<ZoomOut />}
-          aria-label="zoom-out"
-          onClick={() => setZoom(Math.max(zoom - 0.25, MIN_ZOOM))}
-          disabled={zoom === MIN_ZOOM}
-        />
-        <IconButton
-          icon={<ZoomIn />}
-          aria-label="zoom-in"
-          onClick={() => setZoom(Math.min(zoom + 0.25, MAX_ZOOM))}
-          disabled={zoom === MAX_ZOOM}
-        />
-      </ButtonGroup>
-      <ButtonGroup position="absolute" top="70px">
-        <IconButton
-          icon={<ChevronLeft />}
-          aria-label="previous-page"
-          onClick={() => setPageNumber(Math.max(pageNumber - 1, 1))}
-          disabled={pageNumber === 1}
-        />
-        <IconButton
-          icon={<ChevronRight />}
-          aria-label="next-page"
-          onClick={() => setPageNumber(Math.min(pageNumber + 1, totalPages))}
-          disabled={pageNumber === totalPages}
-        />
-      </ButtonGroup>
-    </React.Fragment>
+      <Box
+        className={"react-pdf-nav-container " + (showControls ? "" : "hidden")}
+        ref={navRef}
+      >
+        <ButtonGroup className="react-pdf-btn-group">
+          <IconButton
+            size="sm"
+            icon={<ZoomOut />}
+            title="Zoom out"
+            aria-label="zoom-out"
+            onClick={() => setZoom(Math.max(zoom - 0.25, MIN_ZOOM))}
+            disabled={!showControls || zoom === MIN_ZOOM}
+          />
+          <Text>{Math.floor(zoom * 100)}%</Text>
+          <IconButton
+            size="sm"
+            icon={<ZoomIn />}
+            title="Zoom in"
+            aria-label="zoom-in"
+            onClick={() => setZoom(Math.min(zoom + 0.25, MAX_ZOOM))}
+            disabled={!showControls || zoom === MAX_ZOOM}
+          />
+        </ButtonGroup>
+        <ButtonGroup className="react-pdf-btn-group">
+          <IconButton
+            size="sm"
+            icon={<ChevronLeft />}
+            title="Previous page"
+            aria-label="previous-page"
+            onClick={() => setPageNumber(Math.max(pageNumber - 1, 1))}
+            disabled={!showControls || pageNumber === 1}
+          />
+          <Text>
+            {pageNumber} / {totalPages}
+          </Text>
+          <IconButton
+            size="sm"
+            icon={<ChevronRight />}
+            title="Next page"
+            aria-label="next-page"
+            onClick={() => setPageNumber(Math.min(pageNumber + 1, totalPages))}
+            disabled={!showControls || pageNumber === totalPages}
+          />
+        </ButtonGroup>
+      </Box>
+    </Box>
   );
 };
 
