2b01dcee7920432237cef9a166ef527482d7fd73
Danny <51312422+dchenz@users.noreply.github.com>
2023-01-02 19:52:51 +1100

    feat: Add zoom buttons in PDF viewer

--- web/src/pages/Dashboard/ContentModal/PDFModal.tsx
+++ web/src/pages/Dashboard/ContentModal/PDFModal.tsx
@@ -1,6 +1,9 @@
-import React from "react";
+import { ButtonGroup, IconButton } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { ZoomIn, ZoomOut } from "react-bootstrap-icons";
 import { Document, Page, pdfjs } from "react-pdf";
 import { FileRef } from "../../../types/Files";
+import "./styles.scss";
 
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
 
@@ -9,15 +12,37 @@ type PDFModalProps = {
   file: FileRef;
 };
 
-const PDFModal: React.FC<PDFModalProps> = ({ file, bytes }) => {
+const MIN_ZOOM = 0.5;
+const MAX_ZOOM = 2.0;
+
+const PDFModal: React.FC<PDFModalProps> = ({ bytes }) => {
+  const [zoom, setZoom] = useState(1);
   return (
-    <Document file={bytes}>
-      <Page
-        pageNumber={1}
-        renderTextLayer={false}
-        renderAnnotationLayer={false}
-      />
-    </Document>
+    <React.Fragment>
+      <Document file={bytes}>
+        <Page
+          className="react-pdf-page"
+          pageNumber={1}
+          scale={zoom}
+          renderTextLayer={false}
+          renderAnnotationLayer={false}
+        />
+      </Document>
+      <ButtonGroup position="absolute" top="20px">
+        <IconButton
+          icon={<ZoomOut />}
+          aria-label="zoom-out"
+          onClick={() => setZoom(Math.max(zoom - 0.25, MIN_ZOOM))}
+          disabled={zoom === MIN_ZOOM}
+        />
+        <IconButton
+          icon={<ZoomIn />}
+          aria-label="zoom-in"
+          onClick={() => setZoom(Math.min(zoom + 0.25, MAX_ZOOM))}
+          disabled={zoom === MAX_ZOOM}
+        />
+      </ButtonGroup>
+    </React.Fragment>
   );
 };
 
